876d0261c8479c6d7bf206ad3e6b9b0a5f080474
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2025-01-15 13:57:40 +0800

    removed assertion

--- lab01.srcs/sim_1/new/tb_lab01.vhd
+++ lab01.srcs/sim_1/new/tb_lab01.vhd
@@ -58,83 +58,66 @@ begin
         A <= "00";
         B <= "00";
         wait for 10 ns;
-        assert(less = '0' and equal = '1' and greater = '0')report "test1 pass" severity note;
 
         A <= "00";
         B <= "01";
         wait for 10 ns;
-        assert(less = '1' and equal = '0' and greater = '0')report "test2 pass" severity note;
 
         A <= "00";
         B <= "10";
         wait for 10 ns;
-        assert(less = '1' and equal = '0' and greater = '0')report "test3 pass" severity note;
 
         A <= "00";
         B <= "11";
         wait for 10 ns; 
-        assert(less = '1' and equal = '0' and greater = '0')report "test4 pass" severity note;
 
         A <= "01";
         B <= "00";
         wait for 10 ns;
-        assert(less = '0' and equal = '0' and greater = '1')report "test5 pass" severity note;
-
 
         A <= "01";
         B <= "01";
         wait for 10 ns;
-        assert(less = '0' and equal = '1' and greater = '0')report "test6 pass" severity note;
 
         A <= "01";
         B <= "10";
         wait for 10 ns;
-        assert(less = '1' and equal = '0' and greater = '0')report "test7 pass" severity note;
 
         A <= "01";
         B <= "11";
         wait for 10 ns;
-        assert(less = '1' and equal = '0' and greater = '0')report "test8 pass" severity note;
         
         A <= "10";
         B <= "00";
         wait for 10 ns;
-        assert(less = '0' and equal = '0' and greater = '1')report "test9 pass" severity note;
         
         A <= "10";
         B <= "01";
         wait for 10 ns;
-        assert(less = '0' and equal = '0' and greater = '1')report "test10 pass" severity note;
 
         A <= "10";
         B <= "10";
         wait for 10 ns;
-        assert(less = '0' and equal = '1' and greater = '0')report "test11 pass" severity note;
 
         A <= "10";
         B <= "11";
         wait for 10 ns;
-        assert(less = '1' and equal = '0' and greater = '0')report "test12 pass" severity note;
 
         A <= "11";
         B <= "00";
         wait for 10 ns;
-        assert(less = '0' and equal = '0' and greater = '1')report "test13 pass" severity note;
 
         A <= "11";
         B <= "01";
         wait for 10 ns;
-        assert(less = '0' and equal = '0' and greater = '1')report "test14 pass" severity note;
 
         A <= "11";
         B <= "10";
         wait for 10 ns;
-        assert(less = '0' and equal = '0' and greater = '1')report "test15 pass" severity note;
 
         A <= "11";
         B <= "11";
         wait for 10 ns;
-        assert(less = '0' and equal = '1' and greater = '0')report "test16 pass" severity note;
 
         wait;
     end process;
