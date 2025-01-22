3e19454e24c4dcf184b61620dbe67cb1e72d3e1e
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2025-01-22 23:15:56 +0800

    added formatting

--- lab01.srcs/sim_1/new/tb_lab01.vhd
+++ lab01.srcs/sim_1/new/tb_lab01.vhd
@@ -1,128 +1,133 @@
 ----------------------------------------------------------------------------------
 -- Company: CUHK CSE
 -- Engineer: Yu Ching Hei
--- 
+--
 -- Create Date: 15.01.2025 13:00:23
--- Design Name: 
+-- Design Name:
 -- Module Name: tb_lab01 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
 -- Generation date : 15.1.2025 05:25:54 UTC
 
 library ieee;
-use ieee.std_logic_1164.all;
+  use ieee.std_logic_1164.all;
 
 entity tb_lab01 is
-end tb_lab01;
+end entity tb_lab01;
 
 architecture tb of tb_lab01 is
 
-    component lab01
-        port (A       : in std_logic_vector (1 downto 0);
-              B       : in std_logic_vector (1 downto 0);
-              less    : out std_logic;
-              equal   : out std_logic;
-              greater : out std_logic);
-    end component;
+  component lab01 is
+    port (
+      a       : in    std_logic_vector(1 downto 0);
+      b       : in    std_logic_vector(1 downto 0);
+      less    : out   std_logic;
+      equal   : out   std_logic;
+      greater : out   std_logic
+    );
+  end component lab01;
 
-    signal A       : std_logic_vector (1 downto 0);
-    signal B       : std_logic_vector (1 downto 0);
-    signal less    : std_logic;
-    signal equal   : std_logic;
-    signal greater : std_logic;
+  signal a       : std_logic_vector(1 downto 0);
+  signal b       : std_logic_vector(1 downto 0);
+  signal less    : std_logic;
+  signal equal   : std_logic;
+  signal greater : std_logic;
 
 begin
 
-    dut : lab01
-    port map (A       => A,
-              B       => B,
-              less    => less,
-              equal   => equal,
-              greater => greater);
+  dut : component lab01
+    port map (
+      a       => a,
+      b       => b,
+      less    => less,
+      equal   => equal,
+      greater => greater
+    );
 
-    stimuli : process
-    begin
+  stimuli : process is
+  begin
 
-        A <= "00";
-        B <= "00";
-        wait for 10 ns;
+    a <= "00";
+    b <= "00";
+    wait for 10 ns;
 
-        A <= "00";
-        B <= "01";
-        wait for 10 ns;
+    a <= "00";
+    b <= "01";
+    wait for 10 ns;
 
-        A <= "00";
-        B <= "10";
-        wait for 10 ns;
+    a <= "00";
+    b <= "10";
+    wait for 10 ns;
 
-        A <= "00";
-        B <= "11";
-        wait for 10 ns; 
+    a <= "00";
+    b <= "11";
+    wait for 10 ns;
 
-        A <= "01";
-        B <= "00";
-        wait for 10 ns;
+    a <= "01";
+    b <= "00";
+    wait for 10 ns;
 
-        A <= "01";
-        B <= "01";
-        wait for 10 ns;
+    a <= "01";
+    b <= "01";
+    wait for 10 ns;
 
-        A <= "01";
-        B <= "10";
-        wait for 10 ns;
+    a <= "01";
+    b <= "10";
+    wait for 10 ns;
 
-        A <= "01";
-        B <= "11";
-        wait for 10 ns;
-        
-        A <= "10";
-        B <= "00";
-        wait for 10 ns;
-        
-        A <= "10";
-        B <= "01";
-        wait for 10 ns;
+    a <= "01";
+    b <= "11";
+    wait for 10 ns;
 
-        A <= "10";
-        B <= "10";
-        wait for 10 ns;
+    a <= "10";
+    b <= "00";
+    wait for 10 ns;
 
-        A <= "10";
-        B <= "11";
-        wait for 10 ns;
+    a <= "10";
+    b <= "01";
+    wait for 10 ns;
 
-        A <= "11";
-        B <= "00";
-        wait for 10 ns;
+    a <= "10";
+    b <= "10";
+    wait for 10 ns;
 
-        A <= "11";
-        B <= "01";
-        wait for 10 ns;
+    a <= "10";
+    b <= "11";
+    wait for 10 ns;
 
-        A <= "11";
-        B <= "10";
-        wait for 10 ns;
+    a <= "11";
+    b <= "00";
+    wait for 10 ns;
 
-        A <= "11";
-        B <= "11";
-        wait for 10 ns;
+    a <= "11";
+    b <= "01";
+    wait for 10 ns;
 
-        wait;
-    end process;
+    a <= "11";
+    b <= "10";
+    wait for 10 ns;
 
-end tb;
+    a <= "11";
+    b <= "11";
+    wait for 10 ns;
+
+    wait;
+
+  end process stimuli;
+
+end architecture tb;
 
 -- Configuration block below is required by some simulators. Usually no need to edit.
 
--- lab01.srcs/sources_1/new/lab01.vhd
+++ lab01.srcs/sources_1/new/lab01.vhd
@@ -1,38 +1,45 @@
 ----------------------------------------------------------------------------------
 -- Company: CUHK CSE
 -- Engineer: Yu Ching Hei
--- 
+--
 -- Create Date: 15.01.2025 12:48:55
--- Design Name: 
+-- Design Name:
 -- Module Name: lab01 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+  use ieee.std_logic_1164.all;
+
 entity lab01 is
-  Port (
-    A: in std_logic_vector(1 downto 0);
-    B: in std_logic_vector(1 downto 0);
-    less: out std_logic;
-    equal: out std_logic;
-    greater: out std_logic
+  port (
+    a       : in    std_logic_vector(1 downto 0);
+    b       : in    std_logic_vector(1 downto 0);
+    less    : out   std_logic;
+    equal   : out   std_logic;
+    greater : out   std_logic
   );
-end lab01;
-architecture Behavioral of lab01 is
+end entity lab01;
+
+architecture behavioral of lab01 is
 
 begin
-    less <= '1' when (A < B) else '0';
-    equal <= '1' when (A = B) else '0';
-    greater <= '1' when (A > B) else '0';
-end Behavioral;
+
+  less    <= '1' when (a < b) else
+             '0';
+  equal   <= '1' when (a = b) else
+             '0';
+  greater <= '1' when (a > b) else
+             '0';
+
+end architecture behavioral;
