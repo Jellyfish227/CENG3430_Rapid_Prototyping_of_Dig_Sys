e90f2296132f3d972c1daa59ea6c622daec3ab30
C. H. Yu <jellyfishyu0227@gmail.com>
2025-01-22 06:01:56 +0000

    implementing task2

---
+++ lab02.srcs/sources_1/new/lab02.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 22.01.2025 13:30:44
+-- Design Name: 
+-- Module Name: lab02 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lab02 is
+  Port (
+    A, B: IN STD_LOGIC_VECTOR(2 downto 0);
+    Cin: IN STD_LOGIC;
+    Sum: OUT STD_LOGIC_VECTOR(3 downto 0)
+  );
+end lab02;
+
+architecture Behavioral of lab02 is
+
+component full_adder
+  Port (
+    A, B, Cin: IN STD_LOGIC;
+    Sum, Cout: OUT STD_LOGIC
+  );
+end component;
+signal A0to1: std_logic := '0';
+signal A1to2: std_logic := '0';
+
+begin
+  A0: full_adder port map (A[0] => A, B[0] => B, Cin => Cin, Sum => Sum[0], Cout => A0to1);
+  A1: full_adder port map (A[1] => A, B[1] => B, Cin => Cin, Sum => Sum[1], Cout => A0to1);
+  A3: full_adder port map (A[0] => A, B[0] => B, Cin => Cin, Sum => Sum[0], Cout => A0to1);
+
+end Behavioral;
