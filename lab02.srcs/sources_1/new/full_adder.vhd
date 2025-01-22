c53f552b0c5d6df8a0f239abc6695a8ab66362c5
Jellyfish_Yu <jellyfishyu0227@gmail.com>
2025-01-22 23:05:43 +0800

    applied formatting

--- lab02.srcs/sources_1/new/full_adder.vhd
+++ lab02.srcs/sources_1/new/full_adder.vhd
@@ -1,47 +1,46 @@
 ----------------------------------------------------------------------------------
 -- Company: CUHK CSE
 -- Engineer: Yu Ching Hei
--- 
+--
 -- Create Date: 22.01.2025 12:47:34
--- Design Name: 
+-- Design Name:
 -- Module Name: full_adder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+library ieee;
+  use ieee.std_logic_1164.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- library UNISIM;
+-- use UNISIM.VComponents.all;
 
 entity full_adder is
-  Port (
-    A, B, Cin: in std_logic;
-    Sum, Cout: out std_logic
+  port (
+    a    : in    std_logic;
+    b    : in    std_logic;
+    cin  : in    std_logic;
+    sum  : out   std_logic;
+    cout : out   std_logic
   );
-end full_adder;
+end entity full_adder;
 
-architecture Behavioral of full_adder is
+architecture behavioral of full_adder is
 
 begin
-  Sum <= (A xor B) xor Cin;
-  Cout <= ((A xor B) and Cin) or (A and B);
 
-end Behavioral;
+  sum  <= (a xor b) xor cin;
+  cout <= ((a xor b) and cin) or (a and b);
+
+end architecture behavioral;
--- lab02.srcs/sources_1/new/lab02.vhd
+++ lab02.srcs/sources_1/new/lab02.vhd
@@ -1,58 +1,86 @@
 ----------------------------------------------------------------------------------
 -- Company: CUHK CSE
 -- Engineer: Yu Ching Hei
--- 
+--
 -- Create Date: 22.01.2025 13:30:44
--- Design Name: 
+-- Design Name:
 -- Module Name: lab02 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+  use ieee.std_logic_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+-- use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- library UNISIM;
+-- use UNISIM.VComponents.all;
 
 entity lab02 is
-  Port (
-    A, B: IN STD_LOGIC_VECTOR(2 downto 0);
-    Cin: IN STD_LOGIC;
-    Sum: OUT STD_LOGIC_VECTOR(3 downto 0)
+  port (
+    a   : in    std_logic_vector(2 downto 0);
+    b   : in    std_logic_vector(2 downto 0);
+    cin : in    std_logic;
+    sum : out   std_logic_vector(3 downto 0)
   );
-end lab02;
+end entity lab02;
 
-architecture Behavioral of lab02 is
+architecture behavioral of lab02 is
 
-component full_adder
-  Port (
-    A, B, Cin: IN STD_LOGIC;
-    Sum, Cout: OUT STD_LOGIC
-  );
-end component;
-signal A0to1: std_logic := '0';
-signal A1to2: std_logic := '0';
+  component full_adder is
+    port (
+      a    : in    std_logic;
+      b    : in    std_logic;
+      cin  : in    std_logic;
+      sum  : out   std_logic;
+      cout : out   std_logic
+    );
+  end component full_adder;
+
+  signal a0to1 : std_logic := '0';
+  signal a1to2 : std_logic := '0';
 
 begin
-  A0: full_adder port map (A => A(0), B => B(0), Cin => Cin, Sum => Sum(0), Cout => A0to1);
-  A1: full_adder port map (A => A(1), B => B(1), Cin => A0to1, Sum => Sum(1), Cout => A1to2);
-  A3: full_adder port map (A => A(2), B => B(2), Cin => A1to2, Sum => Sum(2), Cout => Sum(3));
 
-end Behavioral;
+  a0 : component full_adder
+    port map (
+      a    => a(0),
+      b    => b(0),
+      cin  => cin,
+      sum  => sum(0),
+      cout => a0to1
+    );
+
+  a1 : component full_adder
+    port map (
+      a    => a(1),
+      b    => b(1),
+      cin  => a0to1,
+      sum  => sum(1),
+      cout => a1to2
+    );
+
+  a3 : component full_adder
+    port map (
+      a    => a(2),
+      b    => b(2),
+      cin  => a1to2,
+      sum  => sum(2),
+      cout => sum(3)
+    );
+
+end architecture behavioral;
