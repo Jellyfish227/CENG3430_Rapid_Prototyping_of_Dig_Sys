3b3702c0c41294808d75b4536fab17d3117bcccf
C. H. Yu <chyu2@cse.cuhk.edu.hk>
2025-02-12 15:57:26 +0800

    Implement three-state stopwatch with up, down, and stop modes

--- lab04.srcs/constrs_1/new/physical-constr.xdc
+++ lab04.srcs/constrs_1/new/physical-constr.xdc
@@ -4,6 +4,10 @@ create_clock -period 10 [get_ports clk];
 
 set_property IOSTANDARD LVCMOS33 [get_ports btnc];
 set_property PACKAGE_PIN P16 [get_ports btnc];
+set_property IOSTANDARD LVCMOS33 [get_ports btnd];
+set_property PACKAGE_PIN R16 [get_ports btnd];
+set_property IOSTANDARD LVCMOS33 [get_ports btnu];
+set_property PACKAGE_PIN T18 [get_ports btnu];
 
 set_property IOSTANDARD LVCMOS33 [get_ports {output[0]}];
 set_property PACKAGE_PIN T22 [get_ports {output[0]}];
@@ -14,15 +18,15 @@ set_property PACKAGE_PIN U22 [get_ports {output[2]}];
 set_property IOSTANDARD LVCMOS33 [get_ports {output[3]}];
 set_property PACKAGE_PIN U21 [get_ports {output[3]}];
 
-set_property IOSTANDARD LVCMOS33 [get_ports {state}];
-set_property PACKAGE_PIN U14 [get_ports {state}];
+# set_property IOSTANDARD LVCMOS33 [get_ports {state}];
+# set_property PACKAGE_PIN U14 [get_ports {state}];
 
-# set_property IOSTANDARD LVCMOS33 [get_ports {state[2]}];
-# set_property PACKAGE_PIN V14 [get_ports {state[2]}];
-# set_property IOSTANDARD LVCMOS33 [get_ports {state[1]}];
-# set_property PACKAGE_PIN V15 [get_ports {state[1]}];
-# set_property IOSTANDARD LVCMOS33 [get_ports {state[0]}];
-# set_property PACKAGE_PIN W15 [get_ports {state[0]}];
+set_property IOSTANDARD LVCMOS33 [get_ports {state[2]}];
+set_property PACKAGE_PIN U14 [get_ports {state[2]}];
+set_property IOSTANDARD LVCMOS33 [get_ports {state[1]}];
+set_property PACKAGE_PIN U19 [get_ports {state[1]}];
+set_property IOSTANDARD LVCMOS33 [get_ports {state[0]}];
+set_property PACKAGE_PIN W22 [get_ports {state[0]}];
 
 set_property IOSTANDARD LVCMOS33 [get_ports {din[0]}];
 set_property PACKAGE_PIN F22 [get_ports {din[0]}];
--- lab04.srcs/sources_1/new/stopwatch_3_state.vhd
+++ lab04.srcs/sources_1/new/stopwatch_3_state.vhd
@@ -1,39 +1,38 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company:
+-- Engineer:
+--
 -- Create Date: 12.02.2025 14:17:52
--- Design Name: 
+-- Design Name:
 -- Module Name: stopwatch_3_state - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
-
 
 library ieee;
   use ieee.std_logic_1164.all;
-  use IEEE.numeric_std.all;
+  use ieee.numeric_std.all;
 
-entity stopwatch_2_state is
+entity stopwatch_3_state is
   port (
     clk    : in    std_logic;
     din    : in    std_logic_vector(3 downto 0);
     btnc   : in    std_logic;
-    state  : out   std_logic;
+    state  : out   std_logic_vector(2 downto 0);
     output : out   std_logic_vector(3 downto 0)
   );
-end entity stopwatch_2_state;
+end entity stopwatch_3_state;
 
-architecture arch_stopwatch_2_state of stopwatch_2_state is
+architecture arch_stopwatch_3_state of stopwatch_3_state is
 
   component clk_div_1hz is
     port (
@@ -49,13 +48,17 @@ architecture arch_stopwatch_2_state of stopwatch_2_state is
     );
   end component clk_div_4hz;
 
-  signal clk_1hz   : std_logic;
-  signal clk_4hz   : std_logic;
-  signal sig_state : std_logic := '0';
-  signal counter   : std_logic_vector(3 downto 0);
+  signal clk_1hz : std_logic;
+  signal clk_4hz : std_logic;
+  signal counter : integer := 0;
+
+  type state_type is (up, down, stop);
+
+  signal state : state_type := stop;
 
 begin
 
+  -- Clock Dividers
   clk_1hz1 : component clk_div_1hz
     port map (
       clk_in  => clk,
@@ -68,35 +71,74 @@ begin
       clk_out => clk_4hz
     );
 
-  state <= sig_state;
+  -- Concurrent Assignments
+  output <= std_logic_vector(to_unsigned(counter, 4));
+
+  -- State Machine
+  state_disp : process (state) is
+  begin
+
+    case state is
+
+      when up =>
+
+        state <= "100";
+
+      when down =>
+
+        state <= "001";
+
+      when stop =>
+
+        state <= "010";
+
+    end case;
+
+  end process state_disp;
 
   change_state : process (clk_4hz) is
   begin
 
     if (rising_edge(clk_4hz)) then
-      if (btnc = '1') then
-        sig_state <= NOT sig_state;
-      else
-        sig_state <= sig_state;
-      end if;
+      if (state = stop) then
+        if (btnu = '1') then
+          state <= up;
+        elsif (btnd = '1') then
+          state <= down;
+        end if;
+
+      elsif (state = up) then
+        if (btnc = '1') then
+          state <= down;
+        else
+          state <= state;
+        end if;
+
+      elsif (state = down) then
+        if (btnc = '1') then
+          state <= stop;
+        else
+          state <= state;
+        end if;
     end if;
 
   end process change_state;
 
-  counter_proc : process (sig_state, clk_1hz) is
+  counter_proc : process (state, clk_1hz) is
   begin
 
-    if (sig_state = '0') then
+    if (state = stop) then
       counter <= din;
-    else
-      if (rising_edge(clk_1hz)) then
-        counter <= std_logic_vector(unsigned(counter) + 1);
+    elsif (state = up) then
+      if (rising_edge(clk_1hz) and counter < to_integer("1111")) then
+        counter <= counter + 1;
+      end if;
+    elsif (state = down) then
+      if (rising_edge(clk_1hz) and counter > to_integer("0000")) then
+        counter <= counter - 1;
       end if;
     end if;
 
   end process counter_proc;
 
-  output <= counter;
-  state  <= sig_state;
-
-end architecture arch_stopwatch_2_state;
+end architecture arch_stopwatch_3_state;
